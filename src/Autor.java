06037211e1ae6a98962fb53a763eaf902195ec0d
mac <artem.workamail.87@gmail.com>
2024-03-06 00:09:59 +0300

    Готовое ДЗ12

--- src/Autor.java
+++ src/Autor.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Autor {
     private final String firstname;
     private final String lastname;
@@ -9,9 +11,28 @@ public class Autor {
 
     public String getFirstname() {
         return firstname;
+        // getFirsname - передает
     }
 
     public String getLastname() {
         return lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "Автор " + firstname + " " + lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Autor autor = (Autor) o;
+        return Objects.equals(firstname, autor.firstname) && Objects.equals(lastname, autor.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstname, lastname);
     }
 }
--- src/Book.java
+++ src/Book.java
@@ -1,14 +1,15 @@
+import java.util.Objects;
+
 public class Book {
     private String title;
     private int publicationDate;
     private Autor autor;
 
-    public Book(String title, int publicationDate, Autor autor) {
+    public Book (String title, int publicationDate, Autor autor) {
         this.title = title;
         this.publicationDate = publicationDate;
         this.autor = autor;
     }
-
     public String getTitle() {
         return title;
     }
@@ -24,4 +25,21 @@ public class Book {
     public void setPublicationDate(int publicationDate) {
         this.publicationDate = publicationDate;
     }
+    public String toString () {
+        return " Название книги " + this.title + " дата публикации " + this.publicationDate + " автор " + getAutor().getFirstname()+ " " + getAutor().getLastname();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationDate == book.publicationDate && Objects.equals(title, book.title) && Objects.equals(autor, book.autor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, publicationDate, autor);
+    }
+
 }
